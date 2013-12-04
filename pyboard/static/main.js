b6f09b6f1c910a1b3f0c4e62e059b34b5c4bfc61
John Weachock <jweachock@gmail.com>
2013-12-04 17:19:15 -0500

    "Fix" score editing on assignments

--- pyboard/static/main.js
+++ pyboard/static/main.js
@@ -5,17 +5,23 @@ function changeGrade(row_id, action) {
 	var actions = row.children(':nth-child(5)');
 
 	if(action == 'edit') {
-	score.data('oldScore', score.html());
-	message.data('oldMessage', message.html());
+		score.data('oldScore', score.html());
+		message.data('oldMessage', message.html());
 
-	score.html('<input type="text" value="' + score.html() + '">');
-	message.html('<input type="text" value="' + message.html() + '">');
-	actions.html('<a href="javascript:changeGrade(\'' + row_id + '\', \'save\')">save</a> <a href="javascript:changeGrade(\'' + row_id + '\', \'reset\')">cancel</a>');
+		var val = score.html();
+		if(val == '‒') {
+			val = '-1';
+		}
+
+		score.html('<input type="text" value="' + val + '">');
+		message.html('<input type="text" value="' + message.html() + '">');
+		actions.html('<a href="javascript:changeGrade(\'' + row_id + '\', \'save\')">save</a> <a href="javascript:changeGrade(\'' + row_id + '\', \'reset\')">cancel</a>');
 	} else if(action == 'reset') {
 		score.html(score.data('oldScore'));
 		message.html(message.data('oldMessage'));
 		actions.html('<a href="javascript:changeGrade(\'' + row_id + '\', \'edit\')">edit</a>');
 	} else if(action == 'save') {
+		// TODO
 	}
 }
 
