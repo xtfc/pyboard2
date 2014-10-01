49af300f2e3ec27ba277028c870b466afc3a7174
Philip Dexter <philip.dexter@gmail.com>
2014-09-30 23:22:44 -0400

    Steakify project

--- grill/__steak_main__.py
+++ grill/__steak_main__.py
@@ -1,17 +1,14 @@
-import bumpy as b
+import steak as s
 from pyboard.app import app
 
-@b.task
 def setup():
 	'''Run standard setup tasks'''
 	pass
 
-@b.task
 def run():
 	'''Run the production server'''
 	app.run(host='0.0.0.0')
 
-@b.task
 def debug():
 	'''Run the debug server'''
 	app.run(host='0.0.0.0', debug=True)
--- grill/db.py
+++ grill/db.py
@@ -1,15 +1,13 @@
-import bumpy as b
+import steak as s
 from pyboard.app import app
 from pyboard.db import Database
 from pyboard.views import open_sql
 
-@b.task
 def init():
 	db = Database(app.config['DATABASE'])
 	db.executescript(open_sql('schema'))
 	db.close()
 
-@b.task
 def test():
 	db = Database(app.config['DATABASE'])
 	db.executescript(open_sql('test'))
